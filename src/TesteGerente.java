d68e078bf63210987ee623f0fc768aeb9091fe60
lucaspires-source <pires.lucas94@gmail.com>
2021-04-08 15:32:20 -0300

    Aula 02.02 Reescrita de métodos

--- src/TesteGerente.java
+++ src/TesteGerente.java
@@ -1,15 +1,19 @@
-public class TesteFuncionario {
+public class TesteGerente {
     public static void main(String[] args) {
 
-        Funcionario lucas = new Funcionario();
+        Gerente lucas = new Gerente();
         lucas.setNome("Lucas Riberio");
         lucas.setCpf("045797361-76");
         lucas.setSalario(10000.00);
+        lucas.setSenha(123);
         System.out.println(lucas.getNome());
         System.out.println(lucas.getSalario());
         System.out.println(lucas.getCpf());
-
+        System.out.println(lucas.getSenha());
         System.out.println(lucas.getBonificacao());
+        boolean autenticou = lucas.autentica(123);
+
+        System.out.println(autenticou);
 
     }
 }
